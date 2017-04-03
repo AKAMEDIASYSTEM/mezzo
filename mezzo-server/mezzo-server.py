3a6326c60958d81d5ae1194f900cf23233fdb717
AKA <nfeehan@gmail.com>
2017-04-03 14:10:18 -0400

    after stupid delay getting static files executed, dummy template and server now running

--- mezzo-server/mezzo-server.py
+++ mezzo-server/mezzo-server.py
@@ -1,23 +1,58 @@
-import tornado.ioloop
+import os
+import config
+import sys
+import logging
+import datetime
 import tornado.web
+import tornado.ioloop
+import random
+
+DEBUG = True
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_PATH = os.path.join(DIRNAME, 'static')
+TEMPLATE_PATH = os.path.join(DIRNAME, 'template')
+print DIRNAME
+print STATIC_PATH
+print TEMPLATE_PATH
+
+settings = {
+    'debug': True,
+    'template_path': TEMPLATE_PATH,
+    'static_path': STATIC_PATH
+    }
+
+db = {'database':'dummy'}
 
 class MainHandler(tornado.web.RequestHandler):
+    # def initialize(self):
+    #     self.static_path = 'foo'
+
     def get(self):
     	# this is the recurring "show current status" req
     	# poll device-disabled-statuses and return properly-formatted template
-        self.write("I AM MEZZO\rYOU ARE MINE")
+        print STATIC_PATH
+        devices = [{'dev':'alexa','devType':'speechTranscript', 'timeTillReactivate':200},
+        {'dev':'echo','devType':'audioRecord', 'timeTillReactivate':200},
+        {'dev':'dropcam','devType':'camera', 'timeTillReactivate':200}]
+        loader = tornado.template.Loader('templates')
+        self.write(loader.load('swipeTemplate.html').generate(devices=devices))
 
     def post(self):
     	# this is the "update device-disabled list" req
     	# look at payload and alter device-disabled-status list as needed
         self.write("I AM MEZZO\rYOU ARE MINE")
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    # (r"/submit", SubmitHandler),
+    # (r"/", BrowserHandler),
+    # (r"/add", AddUserHandler),
+    # (r"/remove", RemoveUserHandler),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": STATIC_PATH}),
+], db=db, **settings)
+
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
+    application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
